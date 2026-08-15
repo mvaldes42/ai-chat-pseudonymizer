eb3cc591ba699c4761e0b3ab7f2059534c2c8623
mvaldes42 <mvaldes@student.42.fr>
2026-08-15 18:11:55 +0200

    feat: add apollo server and client

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
+import { ApolloProvider } from "@apollo/client/react";
+
+const cache = new InMemoryCache();
+const link = new HttpLink({ uri: "http://localhost:4000/" });
+const client = new ApolloClient({
+  // Provide required constructor fields
+  cache: cache,
+  link: link,
+  // Provide some optional constructor fields
+  clientAwareness: {
+    name: "react-web-client",
+    version: "1.3",
+  },
+  queryDeduplication: false,
+});
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement,
 );
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
