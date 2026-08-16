f2efe375009ca1243b36f16b1d6bd8eea88506b3
mvaldes42 <mvaldes@student.42.fr>
2026-08-16 18:49:47 +0200

    feat: add subscriptions

--- server/server.ts
+++ server/server.ts
@@ -2,6 +2,11 @@ import { ApolloServer } from "@apollo/server";
 import { expressMiddleware } from "@as-integrations/express5";
 import cors from "cors";
 import express from "express";
+import { createServer } from "http";
+import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
+import { makeExecutableSchema } from "@graphql-tools/schema";
+import { WebSocketServer } from "ws";
+import { useServer } from "graphql-ws/use/ws";
 
 // A schema is a collection of type definitions (hence "typeDefs")
 // that together define the "shape" of queries that are executed against
@@ -42,25 +47,47 @@ const resolvers = {
   },
 };
 
+// Create the schema, which will be used separately by ApolloServer and
+// the WebSocket server.
+const schema = makeExecutableSchema({ typeDefs, resolvers });
+// Create an Express app and HTTP server; we will attach both the WebSocket
+// server and the ApolloServer to this HTTP server.
 const app = express();
-
-// The ApolloServer constructor requires two parameters: your schema
-// definition and your set of resolvers.
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
+const httpServer = createServer(app);
+// Create our WebSocket server using the HTTP server we just set up.
+const wsServer = new WebSocketServer({
+  server: httpServer,
+  path: "/subscriptions",
 });
-
-// Note you must call `start()` on the `ApolloServer`
-// instance before passing the instance to `expressMiddleware`
+// Save the returned server's info so we can shutdown this server later
+const serverCleanup = useServer({ schema }, wsServer);
+// Set up ApolloServer.
+const server = new ApolloServer({
+  schema,
+  plugins: [
+    // Proper shutdown for the HTTP server.
+    ApolloServerPluginDrainHttpServer({ httpServer }),
+    // Proper shutdown for the WebSocket server.
+    {
+      async serverWillStart() {
+        return {
+          async drainServer() {
+            await serverCleanup.dispose();
+          },
+        };
+      },
+    },
+  ],
+});
 await server.start();
-// Specify the path where we'd like to mount our server
 app.use(
   "/graphql",
   cors<cors.CorsRequest>(),
   express.json(),
   expressMiddleware(server),
 );
-app.listen(4000, () => {
-  console.log(`🚀 Server ready at: http://localhost:4000/graphql`);
+const PORT = 4000;
+// Now that our HTTP server is fully set up, we can listen to it.
+httpServer.listen(PORT, () => {
+  console.log(`🚀 Server ready at: http://localhost:${PORT}/graphql`);
 });
